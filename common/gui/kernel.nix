ae1b69aff410de384b159f1b2a1f35a64e3cb9bb
Teth-IO <105724285+Teth-IO@users.noreply.github.com>
2026-01-01 18:52:18 +0000

    Update compiler and linker flags in kernel.nix

--- common/gui/kernel.nix
+++ common/gui/kernel.nix
@@ -10,14 +10,21 @@
       stdenv = pkgs.kernllvmPackages.stdenv;
       extraMakeFlags = [
         "LLVM=1"
-        "CC=${pkgs.llvmPackages.clang}/bin/clang"
+        "CC=${pkgs.llvmPackages.clangUseLLVM}/bin/clang"
+        "HOSTCC=${pkgs.llvmPackages.clangUseLLVM}/bin/clang"
+        "HOSTCXX=${pkgs.llvmPackages.clangUseLLVM}/bin/clang++"
         "LD=${pkgs.lld}/bin/ld.lld"
+        "HOSTLD=${pkgs.lld}/bin/ld.lld"
         "AR=${pkgs.llvm}/bin/llvm-ar"
+        "HOSTAR=${pkgs.llvm}/bin/llvm-ar"
         "NM=${pkgs.llvm}/bin/llvm-nm"
+        "STRIP=${pkgs.llvm}/bin/llvm-strip"
+        "OBJCOPY=${pkgs.llvm}/bin/llvm-objcopy"
+        "OBJDUMP=${pkgs.llvm}/bin/llvm-objdump"
+        "READELF=${pkgs.llvm}/bin/llvm-readelf"
         "KCFLAGS+=-march=native"
         "KCFLAGS+=-flto=thin"
-        "KCXXFLAGS+=-march=native"
-        "KCXXFLAGS+=-flto=thin"
+        "KCFLAGS+=-O3"
         "NIX_ENFORCE_NO_NATIVE=0"
       ];
       ignoreConfigErrors = true;
